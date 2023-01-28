25dc6c38b9ad3ccf446491156a598aa06a82be2d
Javokhir <jtursunboev@bk.ru>
2023-01-29 08:49:58 +0900

    task adding done

--- TODO/main.js
+++ TODO/main.js
@@ -2,5 +2,16 @@ document.querySelector("#push").onclick = function () {
   if (document.querySelector("#newTask input").value.length == 0) {
      document.querySelector('.hide').style.display = 'block'
    setTimeout(function(){ document.querySelector('.hide').style.display = 'none'
-  }, 2000)};
-};
+  }, 2000)}
+  else{document.querySelector("#tasks").innerHTML += `
+    <div class='task'> 
+        <span id='taskName'>
+        ${document.querySelector("#newTask input").value}
+        </span>
+    <button class='delete'>
+       <i class="fa-solid fa-trash"></i>
+    </button>
+
+    </div> `
+}
+}
