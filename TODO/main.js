61a4e8cebe712deb656a825715e6baa6a790100d
Javokhir <jtursunboev@bk.ru>
2023-01-29 09:14:04 +0900

    deleting part is done in js

--- TODO/main.js
+++ TODO/main.js
@@ -1,17 +1,25 @@
 document.querySelector("#push").onclick = function () {
   if (document.querySelector("#newTask input").value.length == 0) {
-     document.querySelector('.hide').style.display = 'block'
-   setTimeout(function(){ document.querySelector('.hide').style.display = 'none'
-  }, 2000)}
-  else{document.querySelector("#tasks").innerHTML += `
+    document.querySelector(".hide").style.display = "block";
+    setTimeout(function () {
+      document.querySelector(".hide").style.display = "none";
+    }, 2000);
+  } else {
+    document.querySelector("#tasks").innerHTML += `
     <div class='task'> 
         <span id='taskName'>
         ${document.querySelector("#newTask input").value}
         </span>
     <button class='delete'>
-       <i class="fa-solid fa-trash"></i>
+       s
     </button>
+    </div> `;
 
-    </div> `
-}
-}
+    let current__task = document.querySelector(".delete");
+    for (let i = 0; i < current__task.length; i++) {
+      current__task[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
+  }
+};
