c3cc5886cd7291a348f00aa7307aed5a3ea60c80
Javokhir <jtursunboev@bk.ru>
2023-02-01 10:01:48 +0900

    done

--- BMI Calculator/main.js
+++ BMI Calculator/main.js
@@ -6,5 +6,22 @@ function calculate() {
   document.getElementById("height__value").textContent = height + " cm";
 
   let bmi = (weight / Math.pow(height / 100, 2)).toFixed(1);
-  let result = (document.getElementById("result").textContent = bmi);
+  let result = document.getElementById("result");
+
+  result.textContent = bmi;
+  if (bmi < 18.5) {
+    category = "UnderWeight";
+    result.style.color = "#ffc44d";
+  } else if (bmi > 18.5 && bmi <= 24.9) {
+    category = "Normal Weight";
+    result.style.color = "#0be881";
+  } else if (bmi > 25 && bmi <= 29.9) {
+    category = "Overweight";
+    result.style.color = "#ff884d";
+  } else {
+    category = "Obese";
+    result.style.color = "#ff5e57";
+  }
+
+  document.getElementById("category").textContent = category;
 }
